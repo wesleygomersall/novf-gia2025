f230692bbcfda7debac75b4021132395b8c37273
Wesley Gomersall <gomersallw@gmail.com>
2024-12-03 18:13:20 -0800

    initial script, ignore data

---
+++ investigate_MEF_nfprot.r
@@ -0,0 +1,22 @@
+library(data.table)
+library(tidyverse)
+
+LEN_CUTOFF <- 180 # suggested cutoff: minimum length of beta barrel
+FILE <- '~/Desktop/blue_MEF.tsv'
+
+mydata <- fread(FILE) %>%  
+  mutate(length = nchar(protein)) %>% 
+  filter(length > LEN_CUTOFF) %>% 
+  filter(str_detect(protein, "^M")) # start with Met
+
+mydata %>%
+  ggplot(aes(x=length)) + 
+  geom_histogram() + 
+  ggtitle("Histogram of protein lengths after filtering") + 
+  theme_bw()
+
+mydata %>% 
+  ggplot(aes(x=MEF)) + 
+  geom_histogram() + 
+  ggtitle("Histogram of protein MEF after filtering") + 
+  theme_bw()
